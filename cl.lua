3ee756587d4e257a45df0b6820bc8b624568d612
Ziggy <68012775+Ziggy434@users.noreply.github.com>
2022-05-14 20:42:06 +0800

    added proximity commands

--- cl.lua
+++ cl.lua
@@ -1,10 +1,49 @@
 -- Author: ZiggyTheDev
 
 
--- /me command
-RegisterCommand('me', function(source, args)
-    TriggerServerEvent('me', table.concat(args, " "))
+-- /gme command
+RegisterCommand('gme', function(source, args)
+    TriggerServerEvent('gme', table.concat(args, " "))
 end)
+
+--[[////////////////////////////////////////////////////////]]
+
+-- /me comamnd (proximity)
+RegisterNetEvent('proxMe')
+AddEventHandler('proxMe', function(id, message)
+    local playerId = PlayerId()
+    local senderId = GetPlayerFromServerId(id)
+    local senderName = GetPlayerName(senderId)
+    if senderId == playerId then
+        TriggerClientEvent('chatMessage', "~r~[ME] ".. senderName.. ": ^8".. message))
+    elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(playerId)), GetEntityCoords(GetPlayerPed(senderId)), true) < 19.999 then
+        TriggerClientEvent('chatMessage', "~r~[ME] ".. senderName.. ": ^8".. message))
+    end
+end) 
+
+--[[////////////////////////////////////////////////////////]]
+
+-- /ooc comamnd (proximity)
+
+RegisterNetEvent('proxOoc')
+AddEventHandler('proxOoc', function(id, message)
+    local playerId = PlayerId()
+    local senderId = GetPlayerFromServerId(id)
+    local senderName = GetPlayerName(senderId)
+    if senderId == playerId then
+        TriggerClientEvent('chatMessage', "~b~[OOC] ".. senderName.. ": ^8".. message))
+    elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(playerId)), GetEntityCoords(GetPlayerPed(senderId)), true) < 19.999 then
+        TriggerClientEvent('chatMessage', "~b~[OOC] ".. senderName.. ": ^8".. message))
+    end
+end) 
+
+
+-- /gooc command
+RegisterCommand('gooc', function(source, args)
+    TriggerServerEvent('gooc', table.concat(args, " "))
+end)
+
+--[[////////////////////////////////////////////////////////]]
 
 --[[////////////////////////////////////////////////////////]]
 
